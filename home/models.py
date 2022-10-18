2d9dc7ddd6c0afafdb1544693e54394666359177
Edwin <edwinkiyimba@gmail.com>
2022-10-18 13:09:39 +0300

    Added chill areas and types

--- home/models.py
+++ home/models.py
@@ -1,8 +1,15 @@
+from email.policy import default
 from django.contrib.gis.db import models
 
 from district.models import District, Village
 
 # Create your models here.
+
+
+class ChillType(models.Model):
+    name = models.CharField(max_length=255)
+    incon = models.CharField(max_length=255, default='fa-coffee')
+    color = models.CharField(max_length=255, default='blue')
 
 
 class TouristSites(models.Model):
@@ -21,3 +28,25 @@ class TouristSites(models.Model):
         District, on_delete=models.CASCADE, related_name='sites')
     village = models.ForeignKey(
         Village, on_delete=models.CASCADE, related_name='sites')
+
+
+class Chill(models.Model):
+    contact = models.CharField(max_length=255)
+    checkin = models.CharField(max_length=255)
+    checkout = models.CharField(max_length=255)
+    foods = models.CharField(max_length=255)
+    dishes = models.CharField(max_length=255)
+    sauces = models.CharField(max_length=255)
+    website = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    number_of_beds = models.CharField(max_length=255)
+    number_of_rooms = models.CharField(max_length=255)
+    chill = models.ForeignKey(ChillType, related_name="chills")
+
+
+class Media(models.Model):
+    link = models.CharField(max_length=500)
+    site = models.ForeignKey(TouristSites, blank=True,
+                             null=True, on_delete=models.CASCADE, related_name="media")
+    chill = models.ForeignKey(
+        Chill, blank=True, null=True,  on_delete=models.CASCADE, related_name="media")
